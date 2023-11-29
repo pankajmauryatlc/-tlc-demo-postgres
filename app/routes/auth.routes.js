1fec85685ff165fa967b3d3569f6dcf072fc9ffd
Pankaj Maurya <pankaj.maurya@tlcgroup.net>
2023-11-29 13:20:48 +0530

    code updated

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -3,6 +3,10 @@ const {signin,signout,signup} = require("../controllers/auth.controller");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept"
+    );
     next();
   });
 
