2331b6a6218e72194af9cf2d7ccf03d126aaf506
Pankaj Maurya <pankaj.maurya@tlcgroup.net>
2023-11-29 15:56:07 +0530

    code updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended:   true }));
 
 app.use(
   cookieSession({
